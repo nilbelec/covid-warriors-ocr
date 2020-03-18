8685bfba624a720faf3ae282ce28690a2def5657
Juan Álvarez <celeblin@gmail.com>
2020-03-18 20:35:25 +0100

    Refactoring

--- app.py
+++ app.py
@@ -11,13 +11,9 @@ app.config['JSON_AS_ASCII'] = False
 app.config['MAX_CONTENT_LENGTH'] = 4 * 1024 * 1024
 
 
-def allowed_file(filename):
-	return '.' in filename and filename.rsplit('.', 1)[1] in set(['png', 'jpg', 'jpeg', 'gif', 'tif', 'tiff'])
-
-
 @app.errorhandler(404)
 def not_found(error):
-	resp = jsonify({'status': 404,'message': 'Resource not found'})
+	resp = jsonify({'error': 'Resource not found'})
 	resp.status_code = 404
 	return resp
 
@@ -30,17 +26,17 @@ def index():
 @app.route('/ocr', methods=['POST'])
 def ocr():
 	file = request.files['file']
-	if not file or not allowed_file(file.filename):
-		resp = jsonify({'status': 415,'message': 'Unsupported Media Type'})
-		resp.status_code = 415
-		return resp		
+	if not file:
+		resp = jsonify({'error': 'No File Provided'})
+		resp.status_code = 400
+		return resp
 	try:
 		lines = process_image(file).splitlines()
-		resp = jsonify({'status': 200,'text': {i: l for i, l in enumerate(lines)}})
+		resp = jsonify({'text': {i: l for i, l in enumerate(lines)}})
 		resp.status_code = 200
 		return resp
 	except:
-		resp = jsonify({'status': 422,'message': 'Unprocessable Entity'})
+		resp = jsonify({'error': 'Unprocessable Entity'})
 		resp.status_code = 422
 		return resp
 

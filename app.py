86a7a47780275cd715bd89ce0bedc0c91b438316
Juan Álvarez <celeblin@gmail.com>
2020-03-18 20:16:01 +0100

    Refactoring

--- app.py
+++ app.py
@@ -1,11 +1,7 @@
-import os
-import subprocess
 import sys
 import logging
-import shutil
-import tempfile
 from flask import Flask, jsonify, render_template, request
-from werkzeug.utils import secure_filename
+from ocr import process_image
 
 app = Flask(__name__)
 app.logger.addHandler(logging.StreamHandler(sys.stdout))
@@ -16,49 +12,37 @@ app.config['MAX_CONTENT_LENGTH'] = 4 * 1024 * 1024
 
 
 def allowed_file(filename):
-  return '.' in filename and filename.rsplit('.', 1)[1] in set(['png', 'jpg', 'jpeg', 'gif', 'tif', 'tiff'])
+	return '.' in filename and filename.rsplit('.', 1)[1] in set(['png', 'jpg', 'jpeg', 'gif', 'tif', 'tiff'])
 
 
 @app.errorhandler(404)
 def not_found(error):
-  resp = jsonify({
-      'status': 404,
-      'message': 'Resource not found'
-  })
-  resp.status_code = 404
-  return resp
+	resp = jsonify({'status': 404,'message': 'Resource not found'})
+	resp.status_code = 404
+	return resp
 
 
 @app.route('/', methods=['GET'])
 def index():
-  return render_template('index.html')
+	return render_template('index.html')
 
 
-@app.route('/process', methods=['POST'])
-def process():
+@app.route('/ocr', methods=['POST'])
+def ocr():
 	file = request.files['file']
 	if not file or not allowed_file(file.filename):
 		resp = jsonify({'status': 415,'message': 'Unsupported Media Type'})
 		resp.status_code = 415
+		return resp		
+	try:
+		lines = process_image(file).splitlines()
+		resp = jsonify({'status': 200,'text': {i: l for i, l in enumerate(lines)}})
+		resp.status_code = 200
 		return resp
-
-	folder = tempfile.mkdtemp()
-	input_file = os.path.join(folder, secure_filename(file.filename))
-	file.save(input_file)
-	
-	output_file = os.path.join(folder, 'output')
-	command = ['tesseract', input_file, output_file, '-l', 'spa+eng']
-	subprocess.run(command)
-
-	output_file += '.txt'
-	if os.path.isfile(output_file):
-		with open(output_file, encoding="UTF-8") as f:
-			resp = jsonify({'status': 200,'text': {k: v for k, v in enumerate(f.read().splitlines())}})
-	else:
+	except:
 		resp = jsonify({'status': 422,'message': 'Unprocessable Entity'})
 		resp.status_code = 422
-	shutil.rmtree(folder)
-	return resp
+		return resp
 
 
 if __name__ == '__main__':
---
+++ ocr.py
@@ -0,0 +1,16 @@
+import os
+import subprocess
+import tempfile
+from werkzeug.utils import secure_filename
+
+def process_image(file):
+	folder = tempfile.mkdtemp()
+	input_file = os.path.join(folder, secure_filename(file.filename))
+	file.save(input_file)
+	
+	output_file = os.path.join(folder, 'output')
+	command = ['tesseract', input_file, output_file, '-l', 'spa+eng']
+	subprocess.run(command)
+
+	with open(output_file + '.txt', encoding="UTF-8") as f:
+		return f.read()
